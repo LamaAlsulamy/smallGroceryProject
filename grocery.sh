05ac33360f6975718f7f3063981299f54850eeb3
LamaAlsulamy <s443006533@st.uqu.edu.sa>
2024-05-16 23:40:45 +0300

    third modification

--- grocery.sh
+++ grocery.sh
@@ -18,3 +18,24 @@ calculate_total() {
     done
     echo "Total amount: SAR $total"
 }
+
+# Function to save and print purchase invoice
+save_invoice() {
+    invoice_file="$1"
+    invoice_content="----- Invoice -----\n"
+    invoice_content+="Date: $(date)\n"
+    invoice_content+="Customer: $customer_name\n"
+    invoice_content+="Delivery: $delivery_choice\n"
+    for ((i=0; i<${#selected_products[@]}; i++)); do
+        invoice_content+="${selected_products[$i]} - ${selected_prices[$i]}\n"
+    done
+    invoice_content+="Total amount: SAR $total_amount\n"
+    invoice_content+="-------------------\n"
+
+    # Print the content of the invoice
+    echo -e "$invoice_content"
+
+    # Save the invoice content to a separate file
+    echo -e "$invoice_content" >>"$invoice_file"
+
+}
