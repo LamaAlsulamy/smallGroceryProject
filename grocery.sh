90b2701109cde660da0c17667b230f10c3a25829
LamaAlsulamy <s443006533@st.uqu.edu.sa>
2024-05-16 23:36:41 +0300

    second modification

--- grocery.sh
+++ grocery.sh
@@ -8,3 +8,13 @@ display_products() {
     echo "List of $category:"
     paste -d '\t' "$products_file" "$prices_file" | nl
 }
+
+# calculate the total cost
+calculate_total() {
+    total=0
+    for price in "${selected_prices[@]}"; do
+        price_value=$(echo "$price" | sed 's/SAR //')
+        total=$(bc <<< "$total + $price_value")
+    done
+    echo "Total amount: SAR $total"
+}
